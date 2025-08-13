9f2a199c7a83c6db781603da5b50580181a9fb2f
iHamza7 <khanazam3324@gmail.com>
2025-08-13 14:48:48 +0300

    initial commit of bookspot app

---
+++ android/app/src/main/kotlin/com/example/bookspot/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bookspot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/flavors/main_admin.dart
@@ -0,0 +1,8 @@
+import 'package:bookspot/flavors/global.dart';
+import 'package:bookspot/my_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  Global.baseUrl = "Admin App";
+  runApp(const MyApp());
+}
