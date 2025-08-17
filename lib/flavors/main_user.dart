8eb32581fa79a8cb2bdfd93015867bf19dd933df
iHamza7 <khanazam3324@gmail.com>
2025-08-17 11:11:00 +0300

    added the user

--- lib/flavors/main_user.dart
+++ lib/flavors/main_user.dart
@@ -1,8 +1,12 @@
+import 'package:bookspot/firebase_options.dart';
 import 'package:bookspot/flavors/global.dart';
 import 'package:bookspot/my_app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   Global.baseUrl = "User App";
   runApp(const MyApp());
 }
